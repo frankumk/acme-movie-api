10316f1a24ab512c2d488fbc27df044b133d6187
Kayla Frankum <frankumk@gmail.com>
2021-02-01 18:02:02 -0800

    added test spec and routes

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ app.get('/', (req,res)=>{
         </html>
     `)
 })
+
+app.get('/api/movies', async(req,res)=>{
+    res.status(200).send(await Movie.findAll())
+})
+
+app.get('/api/actors', async(req,res)=>{
+    res.status(200).send(await Actor.findAll())
+})
--- test/spec.js
+++ test/spec.js
@@ -11,4 +11,18 @@ describe('Routes',()=>{
             expect(response.text).to.include('Acme API');
         })
     })
+    describe('GET /api/movies',()=>{
+        it('shows api',async()=>{
+            const response = await app.get('/api/movies');
+            expect(response.status).to.equal(200);
+            expect(response.body.length).to.equal(4);
+        })
+    })
+    describe('GET /api/actors',()=>{
+        it('shows api',async()=>{
+            const response = await app.get('/api/movies');
+            expect(response.status).to.equal(200);
+            expect(response.body.length).to.equal(4);
+        })
+    })
 })
